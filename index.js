de36dcc4d6a3c23b95207bfab2d1bc954da8526d
shinsw627 <shinsw627@naver.com>
2022-02-15 01:00:53 +0900

    모듈화

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const database = require("./database");
-const { ApolloServer, gql } = require("apollo-server");
-const typeDefs = gql`
-  type Query {
-    teams: [Team]
-  }
-  type Team {
-    id: Int
-    manager: String
-    office: String
-    extension_number: String
-    mascot: String
-    cleaning_duty: String
-    project: String
-  }
-`;
-const resolvers = {
-  Query: {
-    teams: () => database.teams,
-  },
-};
-const server = new ApolloServer({ typeDefs, resolvers });
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+const { ApolloServer } = require('apollo-server')
+const _ = require('lodash')
+
+const queries = require('./typedefs-resolvers/_queries')
+const mutations = require('./typedefs-resolvers/_mutations')
+const equipments = require('./typedefs-resolvers/equipments')
+
+const typeDefs = [
+    queries,
+    mutations,
+    equipments.typeDefs,
+]
+
+const resolvers = [
+    equipments.resolvers
+]
+
+const server =  new ApolloServer({typeDefs, resolvers})
+
+server.listen().then(({url}) => {
+    console.log(`🚀  Server ready at ${url}`)
+})
