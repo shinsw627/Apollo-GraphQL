d2cd22241ff9a12e3fc47834d12edbd1ab73a50a
shinsw627 <shinsw627@naver.com>
2022-02-20 19:11:15 +0900

    책 관련 수정

--- books/resolvers/books.resolvers.js
+++ books/resolvers/books.resolvers.js
@@ -2,11 +2,13 @@ const booksRepositories = require("../repositories/books.repositories");
 
 const resolvers = {
   Query: {
+    // 모든 책 조회
     getAllBooks: async (parent, args, context, info) => {
       return await booksRepositories.findAllBooks();
     },
   },
   Mutation: {
+    // 책 추가
     enrollBook: async (parent, args, context, info) => {
       const book = await booksRepositories.createBook(args.bookInfo);
       return book;
